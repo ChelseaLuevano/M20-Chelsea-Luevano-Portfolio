3965b8ba9555f4e6bfe5d3232c3e6189da919bc1
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-06-11 18:46:42 -0400

    added profile pic to About Me page

--- chelsea-luevano-portfolio/src/components/pages/AboutMe.js
+++ chelsea-luevano-portfolio/src/components/pages/AboutMe.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import ProfilePic from './ProfilePic1.jpg'
 
 export default function AboutMe() {
 
     return (
         <div>
             <h2>About Me</h2>
+            <img className='w-1/6' src={ProfilePic} alt="ProfilePic"/>
             <p>I am an Agile Leader currently in Financial Technology Industry. 
                 I am enrolled in a coding bootcamp through Ohio State University 
                 (January 2023 - July 2023).
