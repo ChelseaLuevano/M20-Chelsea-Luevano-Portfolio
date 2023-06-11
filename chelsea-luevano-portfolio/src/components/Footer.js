0a4638ac1ece62767d6a10533f97ef022b6ce7df
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-06-11 11:27:52 -0400

    installed tailwinds css; updaated footer compontent with logos and links, plus styled them with tailwinds

--- chelsea-luevano-portfolio/src/components/Footer.js
+++ chelsea-luevano-portfolio/src/components/Footer.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
+import githubLogo from '../github-mark.svg'
 
 function Footer() {
 
     return (
-        <div>
-            <img className='footer-logo' src="" alt ="GitHub Logo"/>
-            <img className='footer-logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" alt="LinkedIn logo"/>
-        </div>
+        <footer className='flex flex-row place-content-center space-x-10'>
+            <a className=' w-1/12' href="https://github.com/ChelseaLuevano"></a><img  src={githubLogo} alt ="GitHub Logo"/>
+            <a className=' w-1/12' href="https://www.linkedin.com/in/chelsealuevano/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" alt="LinkedIn logo"/></a>
+        </footer>
     )
 }
 
