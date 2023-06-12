a054d4b118d25ea5486ed95c39b8cc5b0375ef84
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-06-12 18:23:41 -0400

    added photos for two projects and updated project info into portfolio page; need to work on layout of portfolio page still so I can size images

--- chelsea-luevano-portfolio/src/components/Footer.js
+++ chelsea-luevano-portfolio/src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
 
     return (
         <footer className='flex flex-row place-content-center space-x-10'>
-            <a className=' w-1/12' href="https://github.com/ChelseaLuevano"></a><img  src={githubLogo} alt ="GitHub Logo"/>
+            <a className=' w-1/12' href="https://github.com/ChelseaLuevano"><img src={githubLogo} alt ="GitHub Logo"/></a>
             <a className=' w-1/12' href="https://www.linkedin.com/in/chelsealuevano/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" alt="LinkedIn logo"/></a>
         </footer>
     )
--- chelsea-luevano-portfolio/src/components/pages/Portfolio.js
+++ chelsea-luevano-portfolio/src/components/pages/Portfolio.js
@@ -1,21 +1,34 @@
 import React, { useState } from 'react';
+import PutASporkInIt from './putasporkinit-co-2023-05-09.png';
+import MoveMeScene from './MoveMe-Scene-Project.png';
 
 export default function Portfolio() {
 
-    const projectTitle = "projectTitle"
+    
     const deployedAppLink = "google.com"
 
     return (
         <div id="Portfolio">
-            <li>
-                <h1>{projectTitle}</h1>
-                <img
-                    alt = "placeholder"
-                    className="project-image"
-                    src={deployedAppLink}
-                    // where/how do I add GitHub Link
-                />
-            </li>    
+            <ul>
+                <li>
+                    <h1>PutASporkInIt</h1>
+                <a className="project " href="https://www.putasporkinit.co/">
+                        <img
+                            src={PutASporkInIt}
+                            alt = "PutASporkInIt Project"
+                    />
+                    </a> 
+                </li>  
+                <li>
+                    <h1>MoveMeScene</h1>
+                <a className="project ">
+                        <img
+                            src={MoveMeScene}
+                            alt = "MoveMeScene Project"
+                    />
+                    </a> 
+                </li> 
+            </ul>     
         </div>
     );
 }
