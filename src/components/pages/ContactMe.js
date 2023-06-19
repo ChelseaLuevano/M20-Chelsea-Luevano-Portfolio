12d66620c32be0517c5ff7f558d4664f0d3af11f
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-06-19 19:26:50 -0400

    worked with the AskBCS to discover that a className of hidden was needed in ContactMe div for it to fix the navigation links when ContactMe is displayed

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -54,7 +54,7 @@ export default function ContactMe() {
         <div id="ContactMe" className="m-10">
             <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
                 <div
-                    className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+                    className=" hidden absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
                     aria-hidden="true"
                 >
                     <div
