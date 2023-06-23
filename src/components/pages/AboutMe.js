c081b1c1e0f856ab11b37377e6c9a78b1565048d
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-06-23 16:51:46 -0400

    updated the styling of AboutMe page a bit to have margin increase, flex styling, font size increase, and rounded corners on image

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -4,13 +4,16 @@ import ProfilePic from './ProfilePic1.jpg'
 export default function AboutMe() {
 
     return (
-        <div id="AboutMe" className="m-10">
-            <h2>About Me</h2>
-            <img className='w-1/6' src={ProfilePic} alt="ProfilePic"/>
-            <p>I am an Agile Leader currently in Financial Technology Industry. 
+        <div id="AboutMe" className="m-20 p-5">
+            <h2 className="text-4xl flex flex-col items-center">About Me</h2>
+            <br></br>
+            <div className="flex flex-row">
+            <img className='w-1/6 rounded-md' src={ProfilePic} alt="ProfilePic"/>
+            <p className=" text-2xl m-10">I am an Agile Leader currently in Financial Technology Industry. 
                 I am enrolled in a coding bootcamp through Ohio State University 
                 (January 2023 - July 2023).
             </p>
+            </div>
         </div>
     );
 }
